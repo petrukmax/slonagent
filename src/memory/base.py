a71e0d09adbbb91b53722fbbd8d833e81e47d406
boomyjee <boomyjee@gmail.com>
2026-02-26 04:33:18 +0300

    feat: add KnowledgeProvider

--- src/memory/base.py
+++ src/memory/base.py
@@ -6,17 +6,18 @@ class BaseProvider(Skill):
     def __init__(self, consolidate_tokens: int = 1_000):
         super().__init__()
         self.consolidate_tokens = consolidate_tokens
-        self._pending_file = os.path.join(Memory.memory_dir, f"PENDING_{type(self).__name__.lower()}.json")
-        self._pending = load_json(self._pending_file,[])
+        if consolidate_tokens > 0:
+            self._pending_file = os.path.join(Memory.memory_dir, f"PENDING_{type(self).__name__.lower()}.json")
+            self._pending = load_json(self._pending_file, [])
 
     async def add_turn(self, turn):
-        if isinstance(turn, dict):
-            self._pending.append(turn)
-            if turn.get("role") == "model":
-                if Memory.count_tokens(self._pending) >= self.consolidate_tokens:
-                    await self._consolidate(self._pending)
-                    self._pending = []
-                save_json(self._pending_file, self._pending)
+        if self.consolidate_tokens == 0 or not isinstance(turn, dict): return
+        self._pending.append(turn)
+        if turn.get("role") == "model":
+            if Memory.count_tokens(self._pending) >= self.consolidate_tokens:
+                await self._consolidate(self._pending)
+                self._pending = []
+            save_json(self._pending_file, self._pending)
 
     async def _consolidate(self, pending):
         pass
