9b1a70bb93f8fa645905aa97e6a9747ff0e11715
boomyjee <boomyjee@gmail.com>
2026-04-07 05:15:29 +0300

    chore: cls before launch in start.bat

--- start.bat
+++ start.bat
@@ -4,4 +4,5 @@ set PYTHONUTF8=1
 if not exist .venv\Lib\site-packages\sitecustomize.py (
     .venv\Scripts\python -c "from pathlib import Path; Path('.venv/Lib/site-packages/sitecustomize.py').write_text('import sys, os\nsys.pycache_prefix = os.path.join(sys.prefix, \"__pycache__\")\n')"
 )
+cls
 .venv\Scripts\python main.py %*
